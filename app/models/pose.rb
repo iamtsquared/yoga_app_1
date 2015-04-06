186ff7dc700dfa9ac47b1253d03b6242aa5f40e3
Tina Ta <iamtsquared@gmail.com>
2015-04-05 22:13:06 -0400

    Add associations

--- app/models/pose.rb
+++ app/models/pose.rb
@@ -1,3 +1,4 @@
 class Pose < ActiveRecord::Base
   belongs_to :sequence
+  has_many :photos
 end
--- app/models/sequence.rb
+++ app/models/sequence.rb
@@ -1,3 +1,6 @@
 class Sequence < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
+  has_many :poses
+  
 end
